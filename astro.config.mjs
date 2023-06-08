092e55f829357d19be68c0b1481594cdbd5913da
Max Knerrich <max.knerrich@outlook.com>
2023-06-08 14:44:21 +0200

    added imagetool

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,20 @@ import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
 import markdoc from '@astrojs/markdoc';
 
+import image from '@astrojs/image';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://knerrich.com',
-	integrations: [sitemap(), markdoc(), UnoCSS()],
-	server: { port: 8894 },
+	integrations: [
+		sitemap(),
+		markdoc(),
+		UnoCSS(),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp',
+		}),
+	],
+	server: {
+		port: 8894,
+	},
 });

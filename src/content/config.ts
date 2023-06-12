7ea15f64913fe428e7a6d642062b9ed12be52485
Max Knerrich <max.knerrich@outlook.com>
2023-06-12 21:29:54 +0200

    made annex draft page

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ const blog = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
+		draft: z.boolean().optional(),
 		// Transform string to Date object
 		pubDate: z
 			.string()
@@ -21,6 +22,7 @@ const projects = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
+		draft: z.boolean().optional(),
 		company: z.string().optional(),
 		role: z.string(),
 		tags: z.string(),
@@ -36,6 +38,7 @@ const skills = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
+		draft: z.boolean().optional(),
 		description: z.string(),
 		link: z.string().url(),
 		// Transform string to Date object

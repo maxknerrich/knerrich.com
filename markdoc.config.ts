31c041cb169708856fbd7d8f32f7bba5a15df53e
Max Knerrich <max.knerrich@outlook.com>
2023-06-14 01:29:09 +0200

    added video component

--- markdoc.config.ts
+++ markdoc.config.ts
@@ -1,16 +1,25 @@
 import Picture from '$components/Picture.astro';
 import Gallery from '$components/Gallery.astro';
+import Video from '$components/Video.astro';
 import Row from '$components/Row.astro';
-import { defineMarkdocConfig, nodes  } from '@astrojs/markdoc/config';
+import { defineMarkdocConfig, nodes } from '@astrojs/markdoc/config';
 
 export default defineMarkdocConfig({
 	nodes: {
 		document: {
 			...nodes.document,
-			render: "main", 
+			render: 'main',
 		},
 	},
 	tags: {
+		video: {
+			render: Video,
+			selfClosing: true,
+			attributes: {
+				src: { type: String, required: true },
+				poster: { type: String },
+			},
+		},
 		picture: {
 			render: Picture,
 			selfClosing: true,
